b2acb46c76d6bb660df2eb6450e126df83c71e38
Veena305 <73694304+Veena305@users.noreply.github.com>
2021-09-08 14:20:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, } from 'react-native';
+import HomeScreen from "./screens/home";
+import IisScreen from './screens/iis';
+import MeteorScreen from './screens/meteors';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator ();
+
+export default function App() {
+  return (
+      <NavigationContainer> 
+        
+        <Stack.Navigator initialRouteName = "Home">
+
+          <Stack.Screen name = "Home"component = {HomeScreen}></Stack.Screen>
+          <Stack.Screen name = "IIS"component = {IisScreen}></Stack.Screen>
+          <Stack.Screen name = "Meteors"component = {MeteorScreen}></Stack.Screen>
+
+        </Stack.Navigator>
+
+      </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
